c98dd88d65ff828dd64806f17f5588b9d6cfb4c2
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-23 12:26:00 +0700

    Add debris with Timer, linearDamping, and angularDamping

--- SpaceRace/GameScene.swift
+++ SpaceRace/GameScene.swift
@@ -19,8 +19,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    let possibleEnemies = ["ball", "hammer", "tv"]
+    var isGameOver = false
+    var gameTimer: Timer?
+    
     override func didMove(to view: SKView) {
         backgroundColor = .black
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
 
         starfield = SKEmitterNode(fileNamed: "starfield")!
         starfield.position = CGPoint(x: 1024, y: 384)
@@ -35,7 +41,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(player)
 
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.position = CGPoint(x: 24, y: 52)
         scoreLabel.horizontalAlignmentMode = .left
         addChild(scoreLabel)
 
@@ -43,6 +49,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         physicsWorld.contactDelegate = self
+    }
+    
+    @objc func createEnemy() {
+        guard let enemy = possibleEnemies.randomElement() else { return }
+
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+        addChild(sprite)
+
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+
+        if !isGameOver {
+            score += 1
+        }
     }
     
 
